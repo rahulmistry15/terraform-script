3c57af524a0b6532f068dcb1cec606d52f25d5c6
rahulmistry15 <130966111+rahulmistry15@users.noreply.github.com>
2023-08-08 12:06:36 +0530

    Add files via upload

---
+++ security group.tf
@@ -0,0 +1,35 @@
+# Security Group
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.webapp-vpc.id
+
+  ingress {
+    description      = "ssh from anywhere"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    description      = "http from anywhere"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Allow_SSH"
+  }
+}
