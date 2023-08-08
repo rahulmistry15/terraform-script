7719c090ca57417f5d1601b066dd86f515c6d607
rahulmistry15 <130966111+rahulmistry15@users.noreply.github.com>
2023-08-08 12:05:32 +0530

    Add files via upload

---
+++ network.tf
@@ -0,0 +1,35 @@
+#creating subnet
+
+resource "aws_subnet" "webapp-subnet-1a" {
+  vpc_id     = aws_vpc.webapp-vpc.id
+  cidr_block = "10.10.0.0/24"
+  availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = true
+  
+  tags = {
+    Name = "Webapp-subnet-1A"
+  }
+}
+
+
+resource "aws_subnet" "webapp-subnet-1b" {
+  vpc_id     = aws_vpc.webapp-vpc.id
+  cidr_block = "10.10.1.0/24"
+  availability_zone = "ap-south-1b"
+  map_public_ip_on_launch = true
+  
+  tags = {
+    Name = "Webapp-subnet-1B"
+  }
+}
+
+
+resource "aws_subnet" "webapp-subnet-1c" {
+  vpc_id     = aws_vpc.webapp-vpc.id
+  cidr_block = "10.10.2.0/24"
+  availability_zone = "ap-south-1c"
+  
+  tags = {
+    Name = "Webapp-subnet-1C"
+  }
+}
