3c4e5dc59867fb5840804ad102c6c2c92f0fb159
rahulmistry15 <130966111+rahulmistry15@users.noreply.github.com>
2023-08-08 12:06:09 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,20 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-bucket-15"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "${var.region}"
+}
